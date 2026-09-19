8009b4be1bf73ffcb1cd7d3fe113d37f6a89d670
sharvarimodi3 <sharvariwork03@gmail.com>
2026-09-19 22:51:36 +0530

    functions and its types

---
+++ functions.py
@@ -0,0 +1,46 @@
+# Function is a reusable block of code 
+# syntax: 
+# def name(parameters):
+#   statements...
+#   retunr value
+
+# def greet():
+#     print("Hello")
+
+# greet()
+# greet()
+
+# Types of Functions : 1. In-built functions 2. User-defined functions
+
+# function with no arguments and no return values:
+# def greet():
+#     print("Welcome")
+#     print("FYMCA")
+# greet()
+
+# # function with arguments and no return value:
+# def add(a, b):
+#     print(f"sum of {a} and {b} is {a+b}")
+#     print(a)
+#     print(b)
+# add(5, 10)
+
+# # function with no arguments no parameteres but with return value
+# def pi():
+#     return 3.14
+# pi()
+# print(pi())
+# # or 
+# x = pi()
+# print(x)
+
+# # function with default argument
+# def greet(name="Sharvari"):
+#     print("Hello", name)
+# greet()
+# greet("Modi")
+
+# # function with arguments
+# def sqaure(n):
+#     return n*n
+# print(sqaure(5))
